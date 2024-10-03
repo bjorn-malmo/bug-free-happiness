6af315778607790152cbfd700f4fa590f29b13b4
Björn Lundmark <bjorn@distributedmedical.com>
2024-10-03 13:06:19 +0200

    Demo project to show inconsistency in CopyTo

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.InteropServices.WindowsRuntime;
+
+var sourceBuffer = new Windows.Storage.Streams.Buffer(128000);
+sourceBuffer.Length = 20000;
+var destBuffer = new Windows.Storage.Streams.Buffer(20000);
+destBuffer.Length = 0;
+
+// This will work using .34 but not .38
+// Please see the reference in the Project file
+sourceBuffer.CopyTo(0, destBuffer, 0, 1000);
+
