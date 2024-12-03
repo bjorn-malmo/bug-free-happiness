8d377782a216190200a92d6d32344330ab4acd4e
Björn Lundmark <bjorn@distributedmedical.com>
2024-12-03 13:52:48 +0100

    Updated example with a CustomControl

---
+++ PropertyBindingTest/TestPage1.xaml.cs
@@ -0,0 +1,21 @@
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+
+namespace PropertyBindingTest;
+/// <summary>
+/// An empty page that can be used on its own or navigated to within a Frame.
+/// </summary>
+public sealed partial class TestPage1 : Page
+{
+    public TestPage1()
+    {
+        this.InitializeComponent();
+
+        MyControl.PropertyChanged += MyControl_PropertyChanged;
+    }
+
+    private void MyControl_PropertyChanged(object? sender, System.ComponentModel.PropertyChangedEventArgs e)
+    {
+        EventFired.Text = "MyControl.PropertyChanged raised";
+    }
+}
